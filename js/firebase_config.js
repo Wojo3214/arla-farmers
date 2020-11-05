7ba1f518915e12634d67d6fdaf81bbb1430fa3bf
Magdalena <polakowska.mag@gmail.com>
2020-11-05 12:35:03 +0100

    Firebase and login functionality

---
+++ js/firebase_config.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// ========== GLOBAL FIREBASE CONFIG ========== //
+
+const _firebaseConfig = {
+    apiKey: "AIzaSyCtHlPXng4y4c1C7yIw3j0DWTlsRlqx2z8",
+    authDomain: "arla-farmers-16126.firebaseapp.com",
+    databaseURL: "https://arla-farmers-16126.firebaseio.com",
+    projectId: "arla-farmers-16126",
+    storageBucket: "arla-farmers-16126.appspot.com",
+    messagingSenderId: "600486602553",
+    appId: "1:600486602553:web:80eaa4e247555dcb4e6ae8",
+    measurementId: "G-X5SWC74RV0"
+};
+
+firebase.initializeApp(_firebaseConfig);
+const _db = firebase.firestore();
---
+++ js/login.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// ========== GLOBAL VARIABLES ========== //
+
+let _currentUser;
+
+
+// ========== FIREBASE AUTH ========== //
+// Listen on authentication state change
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) { // if user exists and is authenticated
+        userAuthenticated(user);
+    } else { // if user is not logged in
+        userNotAuthenticated();
+    }
+});
+
+// === Authenticated user SPA behaviour ==== //
+function userAuthenticated(user) {
+    _currentUser = user;
+    console.log(user);
+
+    // Appending currentUser name ans surname to HTML
+    document.getElementById("hello").innerHTML = "Hi " + user.displayName;
+    document.getElementById("name").innerHTML =
+        `<h2>${user.displayName}</h2>`;
+     
+}
+
+
+//=== New user authentication through email and FB ===/
+function userNotAuthenticated() {
+    _currentUser = null; // reset _currentUser
+    // Firebase UI configuration
+    const uiConfig = {
+        credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+        signInOptions: [
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        ],
+        signInSuccessUrl: '../index.html'
+    };
+    // Init Firebase UI Authentication
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-auth-container', uiConfig);
+}
+
+//=== sign out user ===//
+function logout() {
+    firebase.auth().signOut();
+    navigateToLogin("login")
+}
+
+// navigate to a new view/page by changing href
+function navigateToLogin(pageId) {
+    location.href = "subpages/login.html"
+}
+
+//=== Init function for whole SPA ===//
+function init() {
+    // init user data
+    _userRef.doc(_currentUser.uid).onSnapshot({
+        includeMetadataChanges: true
+    }, function (userData) {
+        if (!userData.metadata.hasPendingWrites && userData.data()) {
+            _currentUser = {
+                ...firebase.auth().currentUser,
+                ...userData.data()
+            }; //concating two objects: authUser object and userData objec from the db
+        }
+    });
+}
