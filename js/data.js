9f196bc96516f146f1563228e6a58c998846c5b4
Magdalena <polakowska.mag@gmail.com>
2020-11-09 13:14:28 +0100

    charts on data page

--- js/data.js
+++ js/data.js
@@ -20,6 +20,9 @@ _dataRef.orderBy("year").onSnapshot(snapshotData => {
     console.log(_carbonFootprint);
 
     appendCarbonFootprint(_carbonFootprint); 
+    appendPieLabels(_carbonFootprint);
+    appendBarCarbon(_carbonFootprint);
+    appendDividedCarbon(_carbonFootprint);
 });
 
 // 2: preparing the data
@@ -129,3 +132,194 @@ function appendCarbonFootprint(carbonFootprint) {
     }
   document.querySelector(".price_number").innerHTML = htmlTemplate;
   }
+
+  /* pie chart */
+
+function prepareDividedCarbon(carbonFootprint) {
+  let diesel = [];
+  let years = [];
+  let electricity = [];
+  let feed = [];
+  let digestion = [];
+  carbonFootprint.forEach(data => {
+      if (data.year === '2019') {
+          diesel.push(data.dieselAll);
+          electricity.push(data.electricityHeatingAll);
+          feed.push(data.importedFeedAll);
+          digestion.push(data.digestionCowsAll);
+          years.push(data.year);
+      }
+  });
+  return {
+      diesel,
+      electricity,
+      feed,
+      digestion,
+      years
+  }
+}
+
+function appendDividedCarbon(carbonFootprint) {
+  let data = prepareDividedCarbon(carbonFootprint);
+  console.log(data);
+  // generate chart
+  let chartContainer = document.querySelector('#carbon_divided');
+  let chart = new Chart(chartContainer, {
+      type: 'doughnut',
+      data: {
+          datasets: [
+              {
+                  data: [data.diesel, data.electricity, data.digestion, data.feed],
+                  fill: true,
+                  label: [data.diesel, data.electricity, data.digestion, data.feed],
+                  borderColor: ["#F3CB54", "#8AB26E", "#C6E1EC", "#2F6F84"],
+                  backgroundColor: ["#F3CB54", "#8AB26E", "#C6E1EC", "#2F6F84"],
+                  pointBorderColor: ["#F3CB54", "#8AB26E", "#C6E1EC", "#2F6F84"],
+                  pointHoverBackgroundColor: "#F3CB54",
+                  pointHoverBorderColor: "#F3CB54",
+              }
+          ],
+      },
+      options: {
+          responsive: true,
+          maintainAspectRatio: true,
+          plugins: {
+              labels: {
+                  render: "100",
+                  fontColor: "black",
+                  fontFamily: "ArlaStrong",
+                  fontSize: 20
+              }
+          }
+      }
+
+  });
+}
+
+/* pie chart labels */
+
+
+function appendPieLabels(carbonFootprint) {
+  let data = prepareDividedCarbon(carbonFootprint);
+  let htmlTemplate = "";
+  htmlTemplate += `
+      <article>
+              <h3 class="legend_header"><span class="diesel_circle"></span>Diesel</h3>
+              <p class="legend_number">${data.diesel} kg</p>
+              <h3 class="legend_header"><span class="electricity_circle"></span>Electricity</h2>
+              <p class="legend_number">${data.electricity} kg</p>
+              <h3 class="legend_header"><span class="digestion_circle"></span>Digestion</h3>
+              <p class="legend_number">${data.digestion} kg</p>
+              <h3 class="legend_header"><span class="feed_circle"></span>Feed</h3>
+              <p class="legend_number">${data.feed} kg</p>
+      </article>
+      `;
+
+  document.querySelector('#pie_legend').innerHTML = htmlTemplate;
+}
+
+
+/* divided bar chart */
+
+function prepareBarCarbon(carbonFootprint) {
+  let diesel = [];
+  let years = [];
+  let electricity = [];
+  let feed = [];
+  let digestion = [];
+  carbonFootprint.forEach(data => {
+      diesel.push(data.dieselAll);
+      electricity.push(data.electricityHeatingAll);
+      feed.push(data.importedFeedAll);
+      digestion.push(data.digestionCowsAll);
+      years.push(data.year);
+
+  });
+  return {
+      diesel,
+      electricity,
+      feed,
+      digestion,
+      years
+  }
+}
+
+function appendBarCarbon(carbonFootprint) {
+  let data = prepareBarCarbon(carbonFootprint);
+  console.log(data);
+
+  let chartContainer = document.querySelector('#carbon_bar');
+  let chart = new Chart(chartContainer, {
+      type: 'bar',
+      data: {
+          datasets: [
+              {
+                  data: data.diesel,
+                  label: 'Diesel',
+                  fill: false,
+                  borderColor: "#F3CB54",
+                  backgroundColor: "#F3CB54",
+                  pointBackgroundColor: "#F3CB54",
+                  pointBorderColor: "#F3CB54",
+                  pointHoverBackgroundColor: "#F3CB54",
+                  pointHoverBorderColor: "#F3CB54",
+              },
+              {
+                  label: 'Electricity and heating',
+                  data: data.electricity,
+                  fill: false,
+                  borderColor: "#8AB26E",
+                  backgroundColor: "#8AB26E",
+                  pointBackgroundColor: "#8AB26E",
+                  pointBorderColor: "#8AB26E",
+                  pointHoverBackgroundColor: "#8AB26E",
+                  pointHoverBorderColor: "#8AB26E",
+                  type: 'bar'
+              },
+              {
+                  label: 'Digestion of cows',
+                  data: data.digestion,
+                  fill: false,
+                  borderColor: "#C6E1EC",
+                  backgroundColor: "#C6E1EC",
+                  pointBackgroundColor: "#C6E1EC",
+                  pointBorderColor: "#C6E1EC",
+                  pointHoverBackgroundColor: "#C6E1EC",
+                  pointHoverBorderColor: "#C6E1EC",
+                  type: 'bar'
+              },
+              {
+                  label: 'Imported feed',
+                  data: data.feed,
+                  fill: false,
+                  borderColor: "#2F6F84",
+                  backgroundColor: "#2F6F84",
+                  pointBackgroundColor: "#2F6F84",
+                  pointBorderColor: "#2F6F84",
+                  pointHoverBackgroundColor: "#2F6F84",
+                  pointHoverBorderColor: "#2F6F84",
+                  type: 'bar',
+                  offset: true,
+
+              }
+          ],
+          labels: data.years,
+
+      },
+      options: {
+          responsive: true,
+          maintainAspectRatio: true,
+          plugins: {
+              labels: {
+                  fontSize: 0
+              }
+          },
+          scales: {
+              xAxes: [{
+                  barThickness: 30
+              }],
+          }
+
+      }
+  });
+}
